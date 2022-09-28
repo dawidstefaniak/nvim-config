2b054577fd15a1736493d646e17951157fa5c6f8
Dawid Stefaniak <dawid.stefaniak@stxgroup.com>
2022-09-28 19:04:43 +0200

    initial commit

---
+++ lua/craftzdog/maps.lua
@@ -0,0 +1,47 @@
+local keymap = vim.keymap
+
+keymap.set('n', 'x', '"_x')
+
+-- Increment/decrement
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+-- Maps for colemak keyboard
+-- arrows
+keymap.set('n', 'j', 'n')
+keymap.set('n', 'n', 'j')
+
+keymap.set('n', 'e', 'k')
+keymap.set('n', 'k', 'e')
+
+keymap.set('n', 'i', 'l')
+keymap.set('n', 'l', 'i')
+
+-- Delete a word backwards
+
+keymap.set('n', 'dw', 'vb"_d')
+
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- Save with root permission (not working for now)
+--vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+
+-- New tab
+keymap.set('n', 'te', ':tabedit')
+-- Split window
+keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+-- Move window
+keymap.set('n', '<Space>', '<C-w>w')
+keymap.set('', 'sh', '<C-w>h')
+keymap.set('', 'sj', '<C-w>k')
+keymap.set('', 'sk', '<C-w>j')
+keymap.set('', 'sl', '<C-w>l')
+
+-- Resize window
+keymap.set('n', '<C-w><left>', '<C-w><')
+keymap.set('n', '<C-w><right>', '<C-w>>')
+keymap.set('n', '<C-w><up>', '<C-w>+')
+keymap.set('n', '<C-w><down>', '<C-w>-')
